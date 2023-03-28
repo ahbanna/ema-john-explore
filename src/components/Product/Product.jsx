571cbeebd88071b952af0267cc543a34142e831d
Al Hasanul Banna <tutul5800@gmail.com>
2023-03-29 00:56:18 +0600

    products details data loaded

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Product.css";
 
 const Product = (props) => {
-  console.log(props.product);
   const { img, name, price, seller, ratings } = props.product;
+  const handleAddToCart = props.handleAddToCart;
+
   return (
     <div className="single-product">
       <img src={img} alt="" />
@@ -13,7 +16,13 @@ const Product = (props) => {
         <p>Manufacturer: {seller}</p>
         <p>Rating: {ratings} starts</p>
       </div>
-      <button className="btn-add-to-cart">Add to Cart</button>
+      <button
+        onClick={() => handleAddToCart(props.product)}
+        className="btn-add-to-cart"
+      >
+        Add to Cart
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
     </div>
   );
 };
